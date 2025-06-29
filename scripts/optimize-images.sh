68952bff76c0efa1cdf8df16973f158b0381f752
Johannes Kamprad <archlinux@kamprad.net>
2025-06-29 13:42:50 +0200

    Update optimize-images.sh

--- scripts/optimize-images.sh
+++ scripts/optimize-images.sh
@@ -4,5 +4,5 @@
 find eos_wallpapers_classic/ eos_wallpapers_community/ -iname "*.jpg" -exec jpegoptim --strip-all --all-progressive {} \;
 
 # Optimize all PNG files in eos_wallpapers_classic and eos_wallpapers_community using zopflipng
-find ./images/ -iname '*.png' -exec zopflipng --iterations=100 --splitting=3 {} {} \;
+find eos_wallpapers_classic/ eos_wallpapers_community/ -iname '*.png' -exec zopflipng --iterations=100 --splitting=3 {} {} \;
 
