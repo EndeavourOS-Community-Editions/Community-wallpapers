bd7ab9ca7e56078f219e55af6cf1f63fd58538d9
Johannes Kamprad <archlinux@kamprad.net>
2025-06-29 13:27:21 +0200

    Update optimize-images.sh

--- scripts/optimize-images.sh
+++ scripts/optimize-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Optimize all JPG files in eos_wallpapers_classic and eos_wallpapers_community using jpegoptim
-find eos_wallpapers_classic/ eos_wallpapers_community/ -iname "*.jpg" -exec jpegoptim --strip-all --all-progressive {} \;
+find eos_wallpapers_classic/ eos_wallpapers_community/ -iname "*.jpg" -exec jpegoptim --strip-all --all-progressive --max=90 {} \;
 
 # Optimize all PNG files in eos_wallpapers_classic and eos_wallpapers_community using oxipng
 find eos_wallpapers_classic/ eos_wallpapers_community/ -iname "*.png" -exec oxipng -o max -strip all {} \;
